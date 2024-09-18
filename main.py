f8d127589b96b4d22ef58da1dc8f318fc33c53eb
Connor Schwab <Connor.l.schwab@gmail.com>
2024-09-18 19:45:57 -0400

    Feat: added a scenario for both second saving and checking account

--- main.py
+++ main.py
@@ -18,11 +18,31 @@ class main:
     print("Reading information from the first checking account. ")
     c1.print_customer_information()
     print("Routing number: " + str(c1._routing_number))
-    transfer = int(input("How much would you like to transfer from checking account"))
+    transfer = int(input("How much would you like to transfer from checking account "))
     if(c1.tranfer(transfer)):
         c1.current_balance -= transfer
         print("Transfer went through account balance has been updated")
     else:
         print("The amount you entered is above the transfer limit for the account please try again later")
+
+    print("Reading information from the second saving account. ")
+    s2.print_customer_information()
+    print("Routing number: " + str(s2._routing_number))
+    print("accumlating a 5% interest for routing number " + str(s2._routing_number) + " for two years")
+    s2.interest_acc()
+    s2.interest_acc()
+    print("Account number " + str(s2._routing_number) + "now has $" + str(s2.interest) + " in interest")
+
+    print("Reading information from the second checking account. ")
+    c2.print_customer_information()
+    print("Routing number: " + str(c2._routing_number))
+    transfer = int(input("How much would you like to transfer from checking account "))
+    if (c2.tranfer(transfer)):
+        c2.current_balance -= transfer
+        print("Transfer went through account balance has been updated")
+    else:
+        print("The amount you entered is above the transfer limit for the account please try again later")
+
+
 
 
