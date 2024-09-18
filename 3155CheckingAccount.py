67074ff0ce03512b84cee0629c58fa54c1dcfcb9
Connor Schwab <Connor.l.schwab@gmail.com>
2024-09-18 15:12:25 -0400

    Feat: made account number a protected member and made trasnfer_max a private memeber

--- 3155CheckingAccount.py
+++ 3155CheckingAccount.py
@@ -1,5 +1,6 @@
 class checking_account(bank):
     def __init__(self, bank, account_number, transfer_max):
         super().__init__(bank)
-        self.account_number = account_number
-        self.transfer_max = transfer_max;
+        self._account_number = account_number
+        self.__transfer_max = transfer_max
+
